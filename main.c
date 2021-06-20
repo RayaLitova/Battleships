05fcc0e7f3b4a4e82e47a5689056f559c4775512
Ad04a <andrey.todorov04@gmail.com>
2021-06-20 17:18:40 +0300

    commitvam koda na sashko che git mu commitna neshto razlichno ot koda

--- main.c
+++ main.c
@@ -3,8 +3,29 @@
  
 extern struct tile_t **map_A_base;
 extern struct tile_t **map_B_base;
+extern struct ship_t *ships_A;
+extern struct ship_t *ships_B;
+extern int pl;
 
-
+int print_map(struct tile_t** map){
+	printf("   |");
+	for(int i=0;i<sqrt(map_size);i++){
+		printf(" %c |", 'A'+i);
+		
+	}
+	for(int i=0;i<sqrt(map_size);i++){
+		printf("\n    --- --- --- --- --- --- --- --- --- ---\n");
+		if(i+1!=10){
+			printf(" %d |", i+1);
+		}else{
+			printf("%d |", i+1);
+		}
+		for(int j=0;j<sqrt(map_size);j++){
+			printf(" %c |", map[i][j].symbol);
+		}
+	}
+	printf("\n");
+}
 
 void choose_difficulty(){
 	printf("Easy(1)\nHard(2)\nInsane(3)\nRandom(4)\n");
@@ -20,8 +41,32 @@ struct tile_t** load_template(int player){
 	struct tile_t  **map=create_empty_map();
 	//load from {player} file
 }
-struct tile_t** random_map(){
+struct tile_t** random_map(player){
+	pl = player;
+	ships_A=malloc(10*sizeof(struct ship_t));
+	ships_B=malloc(10*sizeof(struct ship_t));
 	struct tile_t **map=create_empty_map();
+	printf("Generating random map, please wait\n");
+	int x, y, dir, shipcount = 10, currship = 9, emergency_reset = 0;
+	char directions_help [5] = {'l', 'r', 'u', 'd'};
+	int ships[10] = {2, 2, 2, 2, 3, 3, 3, 4, 4, 6};
+	while (shipcount>0){
+		x = rand() % 10;
+		y = rand() % 10;
+		dir = rand() % 4;
+		printf("x=%d y=%d dir=%c\n", x, y, directions_help[dir]);
+		if(is_suitable(x, y, directions_help[dir], ships[currship], map)==1){
+			place_ship(x, y, directions_help[dir], ships[currship], map);
+			shipcount --;
+			currship--;
+			}
+		if(emergency_reset > 5000){break;}
+		emergency_reset ++;
+	}
+	if (emergency_reset>5000){
+		return random_map(player);}
+	print_map(map);
+	return map;
 }
 
 void choose_map(int player){
@@ -37,12 +82,12 @@ void choose_map(int player){
 		if(player==1) map_A_base=load_template(1);
 		else map_B_base=load_template(2);
 	}else if(map==3){
-		if(player==1) map_A_base=random_map();
-		else map_B_base=random_map();
+		if(player==1) map_A_base=random_map(1);
+		else map_B_base=random_map(2);
 	}else{
 		printf("Invalid option\n");
 		choose_map(player);
-	}
+	};
 }
 
 void game_start(){
@@ -59,12 +104,13 @@ void game_start(){
 	choose_map(1);
 	if(mode==2){
 		choose_map(2);
-	}else if(mode==1){
+	}else{
 		random_map(2);
 	}
 	play();
 }
 
 int main(){
+	struct tile_t** map=create_empty_map();
 	game_start();
 }
